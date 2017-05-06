2d08052e8e371a1cc965832d1e2fe3d56f53d5af
Sebastian Gunner <git@gunner237.dev>
2017-05-06 18:05:47 +0100

    fixing https errors

--- menuBar/responsiveMenuBarBeta.js
+++ menuBar/responsiveMenuBarBeta.js
@@ -1,5 +1,5 @@
 var screenwidth = screen.width;
-document.write('<script src="http://scripts.gunner.online/menuBar/menuBar.js"></script>')
+document.write('<script src="https://scripts.gunner.online/menuBar/menuBar.js"></script>')
 if (screen.width < 800){
     document.write('<script>mobileMB()</script>');
     console.log("Mobile MenuBar applied")
