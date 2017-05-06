a8d63bb21721d8e43c739d5f50c6ece969e7dc81
Sebastian Gunner <git@gunner237.dev>
2017-05-06 14:19:03 +0100

    fixed quote error

--- CSS/responsiveCSS.js
+++ CSS/responsiveCSS.js
@@ -1,11 +1,11 @@
 var screenwidth = screen.width;
 if (screen.width < 800){
     document.write('<link rel="stylesheet" href="http://www.gunner.online/CSS/mobile.css" type="text/css" media="screen" />\
-                   <link rel="stylesheet" href=""http://www.gunner.online/CSS/colour.css" type="text/css" media="screen" />');
+                   <link rel="stylesheet" href="http://www.gunner.online/CSS/colour.css" type="text/css" media="screen" />');
     console.log("Mobile CSS applied")
 }
 else {
-    document.write('<link rel="stylesheet" href=""http://www.gunner.online/CSS/layout.css" type="text/css" media="screen" />\
-                   <link rel="stylesheet" href=""http://www.gunner.online/CSS/colour.css" type="text/css" media="screen" />');
+    document.write('<link rel="stylesheet" href="http://www.gunner.online/CSS/layout.css" type="text/css" media="screen" />\
+                   <link rel="stylesheet" href="http://www.gunner.online/CSS/colour.css" type="text/css" media="screen" />');
     console.log("Desktop CSS applied")
 }
