ec1dff20de6a78cebdb0d01d2addf58cf0f64c2a
Sebastian Gunner <git@gunner237.dev>
2017-05-11 10:59:23 +0100

    Updated beta page screen width limits

--- CSS/responsiveCSSBeta.js
+++ CSS/responsiveCSSBeta.js
@@ -1,5 +1,5 @@
 var screenwidth = screen.width;
-if (screen.width < 800){
+if (screen.width < 750){
     document.write('<link rel="stylesheet" href="https://css.gunner.online/mobile.css" type="text/css" media="screen" />\
                    <link rel="stylesheet" href="https://css.gunner.online/colour.css" type="text/css" media="screen" />');
     console.log("Mobile CSS applied")
