67f19c79f4a3b41b0b86a75ea89d9e335dde52fe
Sebastian Gunner <git@gunner237.dev>
2017-05-11 15:57:04 +0100

    Changed beta GunnerDefaults

--- gunnerDefaultsDev.js
+++
@@ -1,7 +0,0 @@
-console.log("Gunner Intranet development scripts initialising...")
-document.write('<script src="scripts/devCSS.js"></script>');
-console.log("Colour-checker CSS located (1/2)")
-document.write('<script src="scripts/jQuery.js"></script>');
-console.log("jQuery located (2/2)")
-console.log("All scripts located. Loading 2 scripts.")
-//SFTG 2017
