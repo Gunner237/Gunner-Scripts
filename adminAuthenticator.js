22f172e71675095b2a73ba3374cea1406e08e4d0
Sebastian Gunner <git@gunner237.dev>
2017-05-12 14:49:09 +0100

    Updated for spelling error!

--- adminAuthenticator.js
+++ adminAuthenticator.js
@@ -2,13 +2,16 @@
 if (localStorage.adminPriv == "true"){
     if (localStorage.destination == undefined){
         alert("Your destination could not be found. Redirecting to homepage")
-        window.location.href = "index.html"
+        window.location.href = "https://www.gunner.online/"
     }
     else{
             window.location.href = localStorage.destination+".html"
     }
 }
 else {
-    alert("You do not have sufficient priviliages. Please login")
-    window.location.href = "login.html"
+    //alert("You do not have sufficient privileges. Please login")
+    //window.location.href = "login.html"
+    alert("This page is currently unavailable.")
+    window.location.href = "https://www.gunner.online/"
+    
 }
