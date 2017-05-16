32288d3cd17e5c1a595babb26e07a721f61dd8f5
Sebastian Gunner <git@gunner237.dev>
2017-05-16 22:59:29 +0100

    Added French section

--- menuBar/menuBar.js
+++ menuBar/menuBar.js
@@ -255,6 +255,131 @@ function desktopMBde(){
                    ');
     console.log("Desktop (deutsch) version of the Menu Bar has been loaded and applied.")
 }
+//-------------------------------------------FRENCH-------------------------------------
+function desktopMBfr(){
+    var executed = "none";
+    document.write('<ul>\
+                   <li class="dropdown">\
+                   <a href="https://fr.gunner.online/"\
+                   ')
+    if (localStorage.directory == "home"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class="dropbtn"')
+    }
+    document.write('>Page d\'acceil</a>\
+                   <div class="dropdown-content">\
+                   <a href="https://www.gunner.online/aboutIndex">&Uuml;ber die Website</a>\
+                   <a href="https://www.gunner.online/contact">Kontactieren sie mich</a>\
+                   <a href="https://www.gunner.online">Home Page (en-gb)</a>\
+                   ')
+    if (localStorage.betaPriv == "true"){
+        document.write('<a href="https://www.gunner.online/beta/homeBetaRedirect">Beta Home Page</a>\
+                       ');
+    }
+    else{
+    }
+    document.write('</div>\
+                   </li>\
+                   <li class="dropdown">\
+                   <a href="https://www.gunner.online/documents"')
+    if (localStorage.directory == "documents"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class="dropbtn"')
+    }
+    document.write('>Dokumente</a>\
+                   <div class="dropdown-content">\
+                   <a href="https://betasite.gunner.online/academia">Wissenschaft (beta) (en-gb)</a>\
+                   <a href="https://www.gunner.online/downloads">Downloads (en-gb)</a>\
+                   <a href="https://www.gunner.online/techDocs">Technologie Hilfe (en-gb)</a>\
+                   </div>\
+                   </li>\
+                   <li class="dropdown">\
+                   <a href="https://www.gunner.online/games" ')
+    if (localStorage.directory == "games"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class="dropbtn"')
+    }
+    document.write('>Spielen (en-gb)</a>\
+                   <div class="dropdown-content">\
+                   <a href="https://www.gunner.online/numberGuesserLatest">Nummer Rater (en-gb)</a>\
+                   <a href="https://www.gunner.online/piFinder">Pi Sucher (en-gb)</a>\
+                   <a href="https://www.gunner.online/rockPaperScissorsLatest">Schnick Schnack Schnuck (en-gb)</a>\
+                   </div>\
+                   </li>\
+                   <li class="dropdown">\
+                   <a href="https://www.gunner.online/media"')
+    if (localStorage.directory == "media"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class="dropbtn"')
+    }
+    document.write('>Medien (en-gb)</a>\
+                   <div class="dropdown-content">\
+                   <a href="https://www.gunner.online/imageGallery">Fotogalerie (en-gb)</a>\
+                   <a href="https://www.gunner.online/videoGallery">Videogalerie (en-gb)</a>\
+                   </div>\
+                   </li>\
+                   <li class="dropdown">\
+                   <a href="https://www.gunner.online/tools" ')
+    if (localStorage.directory == "tools"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class= "dropbtn"')
+    }
+    document.write('>Tools (en-gb)</a>\
+                   <div class="dropdown-content">\
+                   ')
+    if (localStorage.betaPriv == "true"){
+        document.write('<a href="https://www.gunner.online/hidden/cleanse">Local Storage Cleanser</a>\
+                       <a href="https://www.gunner.online/hidden/maintainance">Maintainance Site</a>\
+                       ')
+    }
+    else{
+    }
+    document.write('<a href="https://www.gunner.online/adminFilter" onClick="return hasher()">MD5 Hasher (en-gb)</a>\
+                   <a href="https://www.gunner.online/adminFilter" onClick="return hasher2()">SHA256 Hasher (en-gb)</a>\
+                   <a href="https://www.gunner.online/primeFinder">Prime Sucher (en-gb)</a>\
+                   </div>\
+                   </li>\
+                   <li class="dropdown">\
+                   <a href="https://www.gunner.online/externalLinks"')
+    if (localStorage.directory == "external"){
+        document.write('class = "active"')
+    }
+    else{
+        document.write('class="dropbtn"')
+    }
+    document.write('>N&uuml;tzliche Webseiten</a>\
+                   <div class="dropdown-content">\
+                   <a href="https://www.google.de">Google</a>\
+                   <a href="https://www.apple.de">Apple</a>\
+                   <a href="https://www.bbc.co.uk">BBC</a>\
+                   <a href="https://www.dropbox.com/login/">Dropbox</a>\
+                   <a href="https://www.linustechtips.com/main">LinusTechTips</a>\
+                   <a href="https://portal.office365.com">Outlook Online</a>\
+                   <a href="https://www.synology.co.uk">Synology</a>\
+                   <a href="https://www.wolframalpha.com">Wolfram Alpha</a>\
+                   <a href="https://www.youtube.com/feed/subscriptions">Youtube</a>\
+                   <a href="https://www.youtube.com/channel/UCicjrjHFJtrN7Gg9OlLG7dg">Youtube Kanal</a>\
+                   </div>\
+                   </li>\
+                   ');
+    document.write('<li style="float:right"><a id = username class="dropbtn" href="https://www.gunner.online">\
+                   Gunner Website (en-gb)\
+                   </a></li>\
+                   </ul>\
+                   ');
+    console.log("Desktop (deutsch) version of the Menu Bar has been loaded and applied.")
+}
+
 //-------------------------------------------THAI-------------------------------------
 function desktopMBth(){
     var executed = "none";
