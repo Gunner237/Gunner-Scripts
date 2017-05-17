57a09be34854044bc63f9f568b927eff57e6995c
Sebastian Gunner <git@gunner237.dev>
2017-05-17 10:48:47 +0100

    Updated latin

---
+++ menuBar/responsiveMenuBarLA.js
@@ -0,0 +1,11 @@
+var screenwidth = screen.width;
+if (screen.width < 800){
+    document.write('<script src="https://scripts.gunner.online/menuBar/menuBar.js"></script>\
+                   <script>mobileMB()</script>');
+    console.log("Mobile MenuBar selected")
+}
+else {
+        document.write('<script src="https://scripts.gunner.online/menuBar/menuBar.js"></script>\
+                       <script>desktopMB("la")</script>');
+        console.log("Desktop MenuBar selected")
+}
