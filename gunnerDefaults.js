218122a1a8a141c7a403f056eb4a1bc07b115a37
Sebastian Gunner <git@gunner237.dev>
2017-05-17 13:52:03 +0100

    Added spanish

--- gunnerDefaults.js
+++ gunnerDefaults.js
@@ -86,6 +86,27 @@ function launch(type){
         current = current + 1
         console.log("Google analytics script gelegen ("+current+"/"+total+")")
     }
+    else if (type == "espanol") {
+        total = 6
+        document.write('<script src="https://scripts.gunner.online/CSS/responsiveCSS.js"></script>');
+        current = current + 1
+        console.log("responsive CSS gelegen ("+current+"/"+total+")")
+        document.write('<script src="https://scripts.gunner.online/error.js"></script>');
+        current = current + 1
+        console.log("error notification gelegen ("+current+"/"+total+")")
+        document.write('<script src="https://scripts.gunner.online/jQuery.js"></script>');
+        current = current + 1
+        console.log("jQuery gelegen ("+current+"/"+total+")")
+        document.write('<script src="https://scripts.gunner.online/email.js"></script>');
+        current = current + 1
+        console.log("Email script gelegen ("+current+"/"+total+")")
+        document.write('<script src="https://scripts.gunner.online/menuBar/responsiveMenuBarES.js"></script>');
+        current = current + 1
+        console.log("Menubar script (th) gelegen ("+current+"/"+total+")")
+        document.write('<script src="https://scripts.gunner.online/googleAnalytics.js"></script>');
+        current = current + 1
+        console.log("Google analytics script gelegen ("+current+"/"+total+")")
+    }
     else if (type == "latin") {
         total = 6
         document.write('<script src="https://scripts.gunner.online/CSS/responsiveCSS.js"></script>');
