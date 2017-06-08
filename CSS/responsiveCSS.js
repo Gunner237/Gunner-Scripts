dbf009ce455361607660b379add9d96fcd439362
Sebastian Gunner <git@gunner237.dev>
2017-06-08 21:11:47 +0100

    Added animation CSS

--- CSS/responsiveCSS.js
+++ CSS/responsiveCSS.js
@@ -6,6 +6,8 @@ if (screen.width < 750){
 }
 else {
     document.write('<link rel="stylesheet" href="https://css.gunner.online/layout/desktop.css" type="text/css" media="screen" />\
-                   <link rel="stylesheet" href="https://css.gunner.online/colour/colour.css" type="text/css" media="screen" />');
+                   <link rel="stylesheet" href="https://css.gunner.online/colour/colour.css" type="text/css" media="screen" />\
+                   <link rel="stylesheet" href="https://css.gunner.online/animations/desktop.css" type="text/css" media="screen" />\
+                   <link rel="stylesheet" href="https://css.gunner.online/animations/keyframes.css" type="text/css" media="screen" />');
     console.log("Desktop CSS applied")
 }
