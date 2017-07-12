df0cfc0c0ae914626cfb7a3f4c59c40e8c710212
Sebastian Gunner <git@gunner237.dev>
2017-07-12 18:42:35 +0100

    Tried moving code

--- gunnerDefaults.js
+++ gunnerDefaults.js
@@ -45,48 +45,6 @@ function launch(type){
         current = current + 1
         console.log("Google analytics script located ("+current+"/"+total+")")
         
-        //New code to determine the page prefix and address (along with protocol used)
-        if (window.jQuery) {
-            var currPage = window.location.href
-            console.log("Current page is "+currPage)
-            
-            // split the string into an array of parts
-            var currPageParts = currPage.split("/");
-            console.log("Current page array is "+currPageParts)
-            if (currPageParts[0] == "https:") {
-                console.log("HTTPS page detected")
-                var pageprefix = currPageParts[2].split(".")
-                console.log("page prefix is "+pageprefix[0])
-            }
-            else if (currPageParts[0] == "file:"){
-                console.log("Local page detected")
-                var thingminus1 = $.inArray("Website", currPageParts)
-                var pageprefix = currPageParts[thingminus1+1].split(".")
-                console.log("page prefix is "+pageprefix[0])
-                
-                
-            }
-            else{
-                console.warn("Page is neither HTTPS, nor a file.")
-                var httpstart = $.inArray("http", currPageParts)
-                
-                if (httpstart == -1){
-                    var pageprefix = currPageParts[0].split(".")
-                    console.log("page prefix is "+pageprefix[0])
-                }
-                else if (httpstart == 0){
-                    var pageprefix = currPageParts[2].split(".")
-                    console.log("page prefix is "+pageprefix[0])
-                }
-                else{
-                    console.error("Page prefix could not be determined")
-                }
-            }
-        }
-        else{
-            console.warn("jQuery not loaded")
-        }
-        
     }
     else if (type == "basic" | type == "lite") {
         total = 3
@@ -317,5 +275,48 @@ function launch(type){
     else{
         console.log("All scripts located. Loading "+current+" scripts.")
     }
+    
+    //New code to determine the page prefix and address (along with protocol used)
+    if (window.jQuery) {
+        var currPage = window.location.href
+        console.log("Current page is "+currPage)
+        
+        // split the string into an array of parts
+        var currPageParts = currPage.split("/");
+        console.log("Current page array is "+currPageParts)
+        if (currPageParts[0] == "https:") {
+            console.log("HTTPS page detected")
+            var pageprefix = currPageParts[2].split(".")
+            console.log("page prefix is "+pageprefix[0])
+        }
+        else if (currPageParts[0] == "file:"){
+            console.log("Local page detected")
+            var thingminus1 = $.inArray("Website", currPageParts)
+            var pageprefix = currPageParts[thingminus1+1].split(".")
+            console.log("page prefix is "+pageprefix[0])
+            
+            
+        }
+        else{
+            console.warn("Page is neither HTTPS, nor a file.")
+            var httpstart = $.inArray("http", currPageParts)
+            
+            if (httpstart == -1){
+                var pageprefix = currPageParts[0].split(".")
+                console.log("page prefix is "+pageprefix[0])
+            }
+            else if (httpstart == 0){
+                var pageprefix = currPageParts[2].split(".")
+                console.log("page prefix is "+pageprefix[0])
+            }
+            else{
+                console.error("Page prefix could not be determined")
+            }
+        }
+    }
+    else{
+        console.warn("jQuery not loaded")
+    }
+
 }
 //SFTG 2017
