0732dd339c224926f9bc3aa74fc3ca7532e94a59
Sebastian Gunner <git@gunner237.dev>
2017-08-08 12:26:09 +0300

    Updated redirect links to use the new protocol

--- menuBar/menuBar.js
+++ menuBar/menuBar.js
@@ -380,25 +380,25 @@ function desktopMB(lang){
     document.write(usefulLinks)
     document.write('</a>\
                    <div class="dropdown-content">\
-                   <a href="https://redirect.gunner.uk/#google.co.uk">Google</a>\
-                   <a href="https://redirect.gunner.uk/#apple.co.uk">Apple</a>\
-                   <a href="https://redirect.gunner.uk/#bbc.co.uk">BBC</a>\
-                   <a href="https://redirect.gunner.uk/#eclecticon.info">Dr French\'s Eclecticon</a>\
-                   <a href="https://redirect.gunner.uk/#linustechtips.com/main">LinusTechTips</a>\
+                   <a href="https://redirect.gunner.uk/google.co.uk">Google</a>\
+                   <a href="https://redirect.gunner.uk/apple.co.uk">Apple</a>\
+                   <a href="https://redirect.gunner.uk/bbc.co.uk">BBC</a>\
+                   <a href="https://redirect.gunner.uk/eclecticon.info">Dr French\'s Eclecticon</a>\
+                   <a href="https://redirect.gunner.uk/linustechtips.com/main">LinusTechTips</a>\
                    <a href="https://www.gunner.cloud">OneDrive</a>\
                    <a href="https://mail.gunner.co.uk">Outlook Web Access</a>\
-                   <a href="https://redirect.gunner.uk/#pcpartpicker.com/user/SftGunner23/saved/">PcPartPicker</a>\
-                   <a href="https://redirect.gunner.uk/#synology.co.uk">Synology</a>\
-                   <a href="https://redirect.gunner.uk/#wolframalpha.com">Wolfram Alpha</a>\
-                   <a href="https://redirect.gunner.uk/#youtube.com/feed/subscriptions">Youtube</a>\
+                   <a href="https://redirect.gunner.uk/pcpartpicker.com/user/SftGunner23/saved/">PcPartPicker</a>\
+                   <a href="https://redirect.gunner.uk/synology.co.uk">Synology</a>\
+                   <a href="https://redirect.gunner.uk/wolframalpha.com">Wolfram Alpha</a>\
+                   <a href="https://redirect.gunner.uk/youtube.com/feed/subscriptions">Youtube</a>\
                    </div>\
                    </li>\
     <li class="dropdown">\
     <a class="dropbtn">Affiliated Sites</a>\
                    <div class="dropdown-content">\
-                   <a href="https://redirect.gunner.uk/#intranet">Intranet</a>\
-                   <a href="https://redirect.gunner.uk/#progsoc.online">ProgSoc</a>\
-                   <a href="https://redirect.gunner.uk/#youtube.com/channel/UCicjrjHFJtrN7Gg9OlLG7dg">')
+                   <a href="https://redirect.gunner.uk/intranet">Intranet</a>\
+                   <a href="https://redirect.gunner.uk/progsoc.online">ProgSoc</a>\
+                   <a href="https://redirect.gunner.uk/youtube.com/channel/UCicjrjHFJtrN7Gg9OlLG7dg">')
     document.write(ytChannel)
     document.write('</a>\
                    </div>\
