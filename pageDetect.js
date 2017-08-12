46aa0bef106c74fc5c9e2d2a9a1d4f6e0e01e51d
Sebastian Gunner <git@gunner237.dev>
2017-08-12 21:28:58 +0300

    Added script to get query string

--- pageDetect.js
+++ pageDetect.js
@@ -40,3 +40,13 @@ if (window.jQuery) {
 else{
     console.warn("jQuery not loaded. Script pageDetect.js did not run sucessfully.")
 }
+
+function getQueryString(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
